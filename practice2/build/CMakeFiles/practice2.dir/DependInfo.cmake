a602eeedd2888625a40a4f32901f9df23e14b0d0
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2023-09-17 13:23:24 +0300

    Remove Build From Ignore

---
+++ practice2/build/CMakeFiles/practice2.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/bogdanspbm/Documents/graphics-course-practice/practice2/main.cpp" "CMakeFiles/practice2.dir/main.cpp.o" "gcc" "CMakeFiles/practice2.dir/main.cpp.o.d"
+  "/Users/bogdanspbm/Documents/graphics-course-practice/practice2/objects/KeyHandler.cpp" "CMakeFiles/practice2.dir/objects/KeyHandler.cpp.o" "gcc" "CMakeFiles/practice2.dir/objects/KeyHandler.cpp.o.d"
+  "/Users/bogdanspbm/Documents/graphics-course-practice/practice2/objects/ProgramAdapter.cpp" "CMakeFiles/practice2.dir/objects/ProgramAdapter.cpp.o" "gcc" "CMakeFiles/practice2.dir/objects/ProgramAdapter.cpp.o.d"
+  "/Users/bogdanspbm/Documents/graphics-course-practice/practice2/utils/ShaderUtils.cpp" "CMakeFiles/practice2.dir/utils/ShaderUtils.cpp.o" "gcc" "CMakeFiles/practice2.dir/utils/ShaderUtils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
