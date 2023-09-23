d5d9c6940e441660a937e7185f3ad43c207676e2
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2023-09-23 20:10:42 +0300

    Task 3 Checkpoint

---
+++ practice3/structures/Vector.h
@@ -0,0 +1,19 @@
+//
+// Created by Bogdan Madzhuga on 23.09.2023.
+//
+
+#ifndef PRACTICE3_VECTOR_H
+#define PRACTICE3_VECTOR_H
+
+#endif //PRACTICE3_VECTOR_H
+
+struct Vector {
+    float x, y, z, w;
+};
+
+
+Vector colorsExampleArray[] = {
+        {0.0f,  1.f,  0.0f, 1.f},
+        {1.f, 0.f, 0.0f, 1.f},
+        {0.f,  0.f, 1.0f, 1.f}
+};
---
+++ practice3/structures/Vertex.h
@@ -0,0 +1,20 @@
+//
+// Created by Bogdan Madzhuga on 23.09.2023.
+//
+
+#ifndef PRACTICE3_VERTEX_H
+#define PRACTICE3_VERTEX_H
+
+
+struct Vertex {
+    float x, y, z;
+};
+
+
+Vertex verticesExampleArray[] = {
+        {0.0f, 100.f, 0.0f},
+        {100.0f, 0.f, 0.0f},
+        {100.f, 100.f, 0.0f}
+};
+
+#endif //PRACTICE3_VERTEX_H
